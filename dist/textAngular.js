59bc1db2356dd00eba3bfe438b6f22d5c498c92f
Suzanne The Marc <marcandre@clickthespot.com>
2017-03-21 12:31:42 -0400

    fixed undefined element reference in handlePopover

--- dist/textAngular.js
+++ dist/textAngular.js
@@ -2991,8 +2991,14 @@ textAngular.directive("textAngular", [
                         if(_resizeTimeout) $timeout.cancel(_resizeTimeout);
                         _resizeTimeout = $timeout(function() {
                             //console.log('resize', scope.displayElements.popover.css('display'));
-                            scope.reflowPopover(scope.resizeElement);
-                            scope.reflowResizeOverlay(scope.resizeElement);
+                            //
+                            if(scope.resizeElement !== undefined) {
+                                
+                                scope.reflowPopover(scope.resizeElement);
+                                scope.reflowResizeOverlay(scope.resizeElement);
+
+                            }
+
                         }, 100);
                     }
                 };
